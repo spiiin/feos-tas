85e512311cd86b7e7a78e802a644b239155c0257
feykomylce <feykomylce@gmail.com>
2012-01-12 17:33:16 +0000

    Upload actual files

---
+++ lua/BattletoadsHitbox.lua
@@ -0,0 +1,58 @@
+-- Battletoads NES
+-- Positions, speeds and hitboxes (not actual bounding boxes, just object frames)
+-- The onscreen data is placed above 8th scanline not to cover the lifebars much
+-- feos, 2011
+
+LastCamX = 0
+
+function ShowBoxes()
+
+   -- Camera position and display
+   CamX = memory.readbyte(0x87) + 256*memory.readbyte(0x88)
+   CamY = memory.readbyte(0x89) + 256*memory.readbyte(0x90)
+   
+   gui.text(170,  1, "CamX " .. CamX)
+   gui.text(170,  9, "CamY " .. CamY)
+   gui.text(170, 17, "CamS " .. CamX - LastCamX) -- X scrolling speed
+   
+   -- Player 1 position
+   X1 = memory.readbyteunsigned(0x3FD) + 256*memory.readbytesigned(0x3EE)
+   Y1 = memory.readbyteunsigned(0x439) + 256*memory.readbytesigned(0x42A)
+   Z1 = memory.readbyteunsigned(0x493) + 256*memory.readbytesigned(0x40C)
+   
+   ScrX1 = X1 - CamX
+   ScrY1 = memory.readbyteunsigned(0x493) - memory.readbyteunsigned(0x475)+8
+   
+   -- Player 2 position   
+   X2 = memory.readbyteunsigned(0x3FE) + 256*memory.readbytesigned(0x3EF)
+   Y2 = memory.readbyteunsigned(0x43A) + 256*memory.readbytesigned(0x42B)
+   Z2 = memory.readbyteunsigned(0x494) + 256*memory.readbytesigned(0x40D)
+   
+   ScrX2 = X2 - CamX
+   ScrY2 = memory.readbyteunsigned(0x494) - memory.readbyteunsigned(0x476)+8
+
+   -- Player 1 display (with subpixels)
+   gui.text(  1,  1, "1X " .. X1 .. "." .. memory.readbyteunsigned(0x448))
+   gui.text(  1,  9, "1Y " .. Y1 .. "." .. memory.readbyteunsigned(0x4CF))
+   gui.text(  1, 17, "1Z " .. Z1 .. "." .. memory.readbyteunsigned(0x457))
+
+   -- Player 2 display (with subpixels)
+   gui.text( 85,  1, "2X " .. X2 .. "." .. memory.readbyteunsigned(0x449))
+   gui.text( 85,  9, "2Y " .. Y2 .. "." .. memory.readbyteunsigned(0x4D0))
+   gui.text( 85, 17, "2Z " .. Z2 .. "." .. memory.readbyteunsigned(0x458))
+   
+   -- Player 1 hitbox, shadow and horizontal speed
+--   gui.box( ScrX1-10, ScrY1-30, ScrX1+10, ScrY1, "#00FF0000")
+--   gui.box( ScrX1-10, memory.readbyteunsigned(0x493)+8, ScrX1+10, memory.readbyteunsigned(0x493)+10, "#00FF0000")
+   gui.text(ScrX1- 9, ScrY1-16,  memory.readbyteunsigned(0x565), "#00ff00ff")
+   
+   -- Player 2 hitbox, shadow and horizontal speed
+--   gui.box( ScrX2-10, ScrY2-30, ScrX2+10, ScrY2, "#FFCC0000")
+--   gui.box( ScrX2-10, memory.readbyteunsigned(0x494)+8, ScrX2+10, memory.readbyteunsigned(0x494)+10, "#FFCC0000")  
+   gui.text(ScrX2- 9, ScrY2-8, memory.readbyteunsigned(0x566), "#FFdd00ff")
+   
+   LastCamX = CamX
+
+end
+
+emu.registerafter(ShowBoxes);
---
+++ lua/TMNT3.lua
@@ -0,0 +1,19 @@
+-- Enemies' slots, HP and invulnerability timers
+
+function info()
+   for i = 0, 7 do
+      x     = memory.readbyte(0x47b+i)
+      y     = memory.readbyte(0x499+i) - memory.readbyte(0x4b7+i)
+      hp    = memory.readbyte(0x622+i)
+      timer = memory.readbyte(0x68a+i)
+	  id    = memory.readbyte(0x52a+i)
+	  
+	  if x<6 then x=6 elseif x>250 then x=250 end
+      if id>0 then
+         gui.text(x-6, y-18, string.format("S%d",i+1))
+         gui.text(x-6, y- 9, string.format(hp))
+         gui.text(x-6, y,    string.format(timer))
+      end
+   end
+end
+emu.registerafter(info);
