e2c1fc06e7334854254425e71ca8ff512e7483a4
feykomylce <feykomylce@gmail.com>
2012-08-17 17:15:26 +0000

    BattletoadsObjects

--- LUA/BattletoadsObjects.lua
+++ LUA/BattletoadsObjects.lua
@@ -1,33 +1,35 @@
 -- feos, 2012
--- Object ID and position display script for NES Batletoads.
+-- Object IDs and atributes display script for NES Batletoads.
 -- Developed with 256x240 screen in mind.
--- Toads objects are omitted.
--- Objects' Y positions repeat during each high byte.
--- This way you can see which of them exist in RAM.
--- 0x4 stands for a blow. When a normal enemy turns into a blow,
--- it means you're moving the right way, i. e. scrolling enemies out.
--- This way you can find checkpoints used for enemies spawns and deletion.
--- 0x42 - restart point.
--- 0x41 - checkpoint to activate.
 
 function info()
 	CamX = memory.readbyte(0x87) + 256*memory.readbyte(0x88)
 	CamY = memory.readbyte(0x89) + 256*memory.readbyte(0x8A)
 
-	LevelCheckpointVal = memory.readbyte(0x585)	
-	gui.text(1,10, "ChP: "..LevelCheckpointVal, "#00ff00ff")
+	LevelCheckpointVal = memory.readbyte(0x585)
+	ObjectPointerLo = memory.readbyte(0xB7)
+	ObjectPointerHi = memory.readbyte(0xB8)
+	gui.box(0,0,256,30,"#000000ff")
+	gui.text(130,231, "ChP: "..string.format("%02X",LevelCheckpointVal), "#00ff00ff")
+	gui.text(180,231, "ObjP: "..string.format("%02X%02X",ObjectPointerHi,ObjectPointerLo), "#00ff00ff")
+	gui.text(1, 1, "id:\nmv:\nlr:\nld:")
 	
-	for i = 2, 14 do
-		x  = memory.readbyte(0x3FD+i) + 256*memory.readbytesigned(0x3EE +i) - CamX
-		y  = memory.readbyte(0x493+i) - memory.readbyte(0x475+i)
-		hp = memory.readbyte(0x51a+i)
-		id = memory.readbyte(0x3c1+i)
+	for i = 0, 14 do
+		id		= memory.readbyte(0x3c1+i)
+		x		= memory.readbyte(0x3FD+i) + 256*memory.readbytesigned(0x3EE +i) - CamX
+		y		= memory.readbyte(0x493+i) - memory.readbyte(0x475+i)
+		move	= memory.readbyte(0x4A2+i)
+		state	= memory.readbyte(0x4B1+i)
+		hp		= memory.readbyte(0x51a+i)
+		linked	= memory.readbyte(0x529+i)
+		linker	= memory.readbyte(0x538+i)
+		follow	= memory.readbyte(0x592+i)
 		
-		if x<0 then x=0 elseif x>242 then x=242 end	
-		gui.text(1+i*18-35, 1, string.format("%X",id))
 		if id>0 then
-			gui.text(x, y, string.format("%X",id))			
-		end
+			gui.text(x, y, string.format("%2X",id))			
+		end		
+		if x<0 then x=0 elseif x>242 then x=242 end	
+		gui.text(1+i*16+18, 1, string.format("%2X\n%2X\n%2X\n%2X",id,state,linker,linked))
 	end
 end
 emu.registerafter(info);
