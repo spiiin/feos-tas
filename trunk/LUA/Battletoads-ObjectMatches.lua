90f3b16c63a1477d4a8ad3e7076112fba801844a
feykomylce@gmail.com <feykomylce@gmail.com@c5f8e67d-12aa-7cd9-4e59-70175b652278>
2012-09-07 16:57:21 +0000

    LUA/Battletoads-ObjectMatches.lua

---
+++ trunk/LUA/Battletoads-ObjectMatches.lua
@@ -0,0 +1,75 @@
+-- Virtual breakpoints table
+-- Comment out the unused
+-- Don't forget the comma when using >1
+--	{  ID, "Attrib",Value}
+Check = {
+	{   1, "Xspd",   0xC0}
+--	{0x22, "Target", 0x55}
+--	{0x7E, "Cntr2",  0x7F}
+--	{0x7F, "VarFlg", 0x7F}
+}
+
+Attribs = {
+--  "Name", offset
+	"Anim_1", -- 1
+	"Cntr_1", -- 2
+	"Xpos_H", -- 3
+	"Xpos_L", -- 4
+	"Ypos_H", -- 5
+	"Ypos_L", -- 6
+	"Zpos_H", -- 7
+	"Zpos_L", -- 8
+	"Xsub",   -- 9
+	"Ysub",   -- 10
+	"Zsub",   -- 11
+	"Zshad",  -- 12
+	"Xshad",  -- 13
+	"Yshad",  -- 14
+	"Flag",   -- 15
+	"State",  -- 16
+	"Zspd",   -- 17
+	"Unk_1",  -- 18
+	"Cntr_2", -- 19
+	"Unk_2",  -- 20 
+	"HitID",  -- 21
+	"HitTmr", -- 22
+	"HP",     -- 23
+	"Linked", -- 24
+	"Linker", -- 25
+	"AnmTmr", -- 26
+	"Unk_3",  -- 27
+	"Xspd",   -- 28
+	"DthTmr", -- 29
+	"Anim_2", -- 30
+	"Target", -- 31
+	"VarFlg", -- 32
+	"Unk_4",  -- 33
+	"EndTmr", -- 34
+}
+
+function Draw()
+	for Slot = 0, 14 do
+		ID	= memory.readbyte(0x3c1+Slot)
+		gui.text(1, 1, "Slot:\nID:")
+		gui.text(1+Slot*15+31, 1,  string.format("%2d",Slot+1))
+		gui.text(1+Slot*15+31, 10, string.format("%2X",ID))		
+		for i,v in ipairs(Check) do
+			for offset,name in ipairs(Attribs) do
+				if ID == v[1] and name == v[2] then
+					gui.text(1, 8+10*i, v[2]..":")
+					CheckAddr = 0x3c1+offset*15+Slot
+					CheckVal = memory.readbyte(CheckAddr)
+					gui.text(1+Slot*15+31, 8+10*i,
+							string.format("%2X",CheckVal),"#00ff00ff")
+					if CheckVal == v[3] then
+						gui.text(100, 231, "ID:"..Slot+1 .." "..v[2].." = "..
+								string.format("%2X!",CheckVal),"white","black")
+						emu.pause()
+					end
+				end
+			end
+		end
+	end
+end
+
+emu.registerafter(Draw);
