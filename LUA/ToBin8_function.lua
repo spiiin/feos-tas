290a61b6a9e4c9065f2cc38329e31d9c3f064a58
feykomylce <feykomylce@gmail.com>
2012-09-08 08:21:53 +0000

    Written ToBin8 convertor function implementation

---
+++ LUA/ToBin8_function.lua
@@ -0,0 +1,16 @@
+function ToBin8(Num,Switch)
+-- 1 byte to binary convertor by feos, 2012
+-- Switch: "s" for string, "n" for number
+	Bin = ""
+	while Num > 0 do
+		Bin = tostring(Num % 2)..Bin
+		Num = math.floor(Num / 2)
+	end
+	Low = string.format("%04d",(Bin % 10000))
+	High = string.format("%04d",math.floor(Bin / 10000))
+	
+	if Switch == "s" then return High.." "..Low
+	elseif Switch == "n" then return Bin
+	else return "Wrong Switch parameter!\nUse \"s\" or \"n\"."
+	end
+end
